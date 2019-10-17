40ac00a159bc07da76fd89eda9407ec5b9c2b6e2
Michael Beemer <michael.beemer@dynatrace.com>
2019-10-17 11:20:22 -0400

    Fixed build issues

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,16 @@ module.exports = {
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.tsx?$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        exclude: [
+          path.resolve(__dirname, "test")
+        ],
+        loader: 'ts-loader'
+      },
     ],
   },
 };
